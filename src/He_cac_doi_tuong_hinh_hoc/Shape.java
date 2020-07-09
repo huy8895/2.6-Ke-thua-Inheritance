080add253ea518bbc8420daed7418313cd31864a
huy8895 <huy8895@gmail.com>
2020-07-09 11:28:15 +0700

    [Thực hành] Hệ các đối tượng hình học

---
+++ src/He_cac_doi_tuong_hinh_hoc/Shape.java
@@ -0,0 +1,44 @@
+package He_cac_doi_tuong_hinh_hoc;
+
+public class Shape {
+    String color;
+    Boolean filled;
+
+    public Shape() {
+        this.color = "green";
+        this.filled = true;
+
+    }
+
+    public Shape(String color, Boolean filled) {
+        this.color = color;
+        this.filled = filled;
+    }
+
+    public String getColor() {
+        return color;
+    }
+
+    public void setColor(String color) {
+        this.color = color;
+    }
+
+    public Boolean getFilled() {
+        return filled;
+    }
+
+    public void setFilled(boolean filled) {
+        this.filled = filled;
+    }
+    public boolean isFilled() {
+        return filled;
+    }
+
+    @Override
+    public String toString() {
+        return "A Shape with color of "
+                + getColor()
+                + " and "
+                + (isFilled() ? "filled" : "not filled");
+    }
+}
