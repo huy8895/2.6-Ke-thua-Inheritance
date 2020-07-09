524acd4a63845ac7e13b172a3e037404923ab003
huy8895 <huy8895@gmail.com>
2020-07-09 20:20:21 +0700

    [**Bài tập] Thiết kế và triển khai lớp Triangle va kiem thu

---
+++ src/thietkevatrienkhailoptriangle/TestTriangle.java
@@ -0,0 +1,12 @@
+package thietkevatrienkhailoptriangle;
+
+public class TestTriangle {
+    public static void main(String[] args) {
+        Triangle triangle = new Triangle();
+        System.out.println(triangle);
+
+        triangle = new Triangle(1,2,3);
+        System.out.println(triangle);
+
+    }
+}
