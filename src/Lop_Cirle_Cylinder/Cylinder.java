36bc348594709fb5ab8b97a5a062916076c88c8d
huy8895 <huy8895@gmail.com>
2020-07-09 14:24:59 +0700

    [Bài tập] Lớp Circle và lớp Cylinder

---
+++ src/Lop_Cirle_Cylinder/Cylinder.java
@@ -0,0 +1,35 @@
+package Lop_Cirle_Cylinder;
+
+public class Cylinder extends Circle {
+    protected double height;
+
+    public Cylinder(){
+
+    }
+    public Cylinder(double radius,double height){
+        super(radius);
+        this.height = height;
+    }
+
+    public double getHeight() {
+        return height;
+    }
+
+    public void setHeight(double height) {
+        this.height = height;
+    }
+
+    public double getVolume(){
+        return super.getArea()*this.height;
+    }
+
+    @Override
+    public String toString(){
+        return "this Cylinder: radius "
+                + this.getRadius()
+                + "height :"
+                + this.getHeight()
+                + " this Volume : "
+                + this.getVolume();
+    }
+}
